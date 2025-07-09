9f59304a66471dd89074f6bfc0544366b5032b1e
irel12 <casaltairel12@gmail.com>
2025-07-09 13:04:47 +0800

    code update

--- database/migrations/2025_07_08_122036_create_studentrecords_table.php
+++ database/migrations/2025_07_08_122036_create_studentrecords_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->integer('total');
             $table->softDeletes();
             $table->timestamps();
-        });
+        }); 
     }
 
     /**
